491939396e28db5d35b60d3a9d3b72983205abfa
simonenkonikita <63547419+simonenkonikita@users.noreply.github.com>
2025-11-01 21:59:44 +0300

    v5

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'node:path';
 
-import { configProvider } from './app.config.provider';
 import { FilmsController } from './films/films.controller';
 import { OrderController } from './order/order.controller';
 import { FilmsService } from './films/films.service';
@@ -13,23 +12,25 @@ import { AppRepository } from './repository/app.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Films } from './entities/films.entity';
 import { Schedules } from './entities/schedule.entity';
+import { envValidationSchema } from '../config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+      validationSchema: envValidationSchema,
     }),
     // Подключаем postgres
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT) || 5432,
-      username: process.env.DATABASE_USERNAME || 'student',
-      password: process.env.DATABASE_PASSWORD || 'student',
-      database: process.env.DATABASE_NAME || 'nest_project',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Films, Schedules],
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'production',
     }),
     // Регистрируем схемы
     TypeOrmModule.forFeature([Films, Schedules]),
@@ -41,6 +42,6 @@ import { Schedules } from './entities/schedule.entity';
     }),
   ],
   controllers: [FilmsController, OrderController],
-  providers: [configProvider, FilmsService, OrderService, AppRepository],
+  providers: [FilmsService, OrderService, AppRepository],
 })
 export class AppModule { }
